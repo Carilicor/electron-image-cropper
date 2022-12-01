b6cb5182ed58bb8d6b9ed16b9d6271cf45cb6327
Cari Licor <Carilicor@gmail.com>
2022-12-01 09:40:13 -0500

    display image data

--- src/renderer/components/PhotoCrop.tsx
+++ src/renderer/components/PhotoCrop.tsx
@@ -1,10 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react'
+import { readFile } from '../../main/helpers'
 
 export default function PhotoCrop() {
-  return (
-    <>
+  const [imageSrc, setImageSrc] = useState(null)
+  const [filename, setFilename] = useState(null)
+  const handleFileChange = async (e: any) =>{
+    if(e.target.files && e.target.files.length) {
+      //we got a file
+      const file = e.target.files[0]
+      setFilename(file.path)
+      //get the image date from a file..
+      const imageData: any = await readFile(file);
+      setImageSrc(imageData)
+    }
+  } 
+   if(!imageSrc) {
+    return (
+      <>
     <h1>PLease Choose a photo to crop</h1>
-    <input type='file' accept="image/*" />
+    <input type='file' accept="image/*" onChange={handleFileChange} />
+   
+    </>
+    )
+  }
+  return(
+    <>
+    <img src={imageSrc} alt="" />
     </>
   )
 }
+
